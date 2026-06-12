7bbb4d7c911fee66d326487b468385a9467b8fc9
fernandolievano <fernandolievan@gmail.com>
2026-06-12 18:30:11 -0300

    feat: path para dataset transformado en config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 # PATHS
-DATA_PATH='data/raw/spotify-tracks-dataset.csv'
-OUTPUT_PATH='outputs/'
+DATA_PATH = "data/raw/spotify-tracks-dataset.csv"
+OUTPUT_PATH = "outputs"
+DATA_TRANSFORMED_PATH = f"{OUTPUT_PATH}/transformed_dataset.csv"
+
 # SPARK
-SPARK_APP_NAME='DESEMPEÑO 2 - PROCESAMIENTO DE DATOS'
+SPARK_APP_NAME = "PARCIAL 2 - PROCESAMIENTO DE DATOS"
